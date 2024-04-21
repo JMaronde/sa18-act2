f610c5b46136c63ae4efb3debdb9bbbd76d38545
Jonathan Maronde <j2mike4@gmail.com>
2024-04-21 14:25:34 -0500

    Created seed data for 3 products (video games) for my game store. Included in each seed the name, price, and description.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+# video games
+Product.create!(
+  name: 'Super Sloth Bros',
+  price: 39.99,
+  description: '2D platforming game where you play as a sloth going through 8 different fun and unique worlds'
+)
+
+Product.create!(
+  name: 'The Legend of the Sloth',
+  price: 59.99,
+  description: '3rd person rpg game where you play as a sloth warrior that aims to get revenge on the dark sloth lord'
+)
+
+Product.create!(
+  name: 'Sloth Kart',
+  price: 99.99,
+  description: 'Action packed racing game where all the characters are sloths, and every vehicle only moves at 10mph'
+)
